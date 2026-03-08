a7e3fff1681f216819f9577ee8fdf21d6441b044
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2026-03-08 04:06:50 -0400

    Fix: Refatoração de página

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
-const menu = document.querySelector(".menu-hamburguer");
-const select_list = document.querySelector(".select-section");
-const sections = document.querySelectorAll("section")
+const roles = [
+  "Fullstack Developer...",
+  "Automation Engineer...",
+  "Backend Specialist...",
+  "Support Computing..."
+];
 
-menu.onclick = () => {
-  select_list.classList.toggle("active")
-  menu.classList.toggle("close-path")
+const typingElement = document.getElementById("typing");
+const errorButtons = document.querySelectorAll(".error");
 
-  sections.forEach((section) => {
-    section.classList.toggle("blur")
-  })
-  console.log(menu)
 
+let roleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+  const currentRole = roles[roleIndex];
+
+  if (deleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  typingElement.textContent = currentRole.slice(0, charIndex);
+
+  let speed = deleting ? 50 : 100;
+
+  if (!deleting && charIndex === currentRole.length) {
+    deleting = true;
+    speed = 1500;
+  }
+
+  if (deleting && charIndex === 0) {
+    deleting = false;
+    roleIndex = (roleIndex + 1) % roles.length;
+  }
+
+  setTimeout(typeEffect, speed);
 }
+
+function errorAlert(msg) {
+  alert(msg)
+}
+
+
+errorButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    errorAlert("Repositório privado");
+  });
+});
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show")
+    }
+  })
+})
+
+document.querySelectorAll(".fade-up").forEach(el => {
+  observer.observe(el)
+})
+
+typeEffect();
